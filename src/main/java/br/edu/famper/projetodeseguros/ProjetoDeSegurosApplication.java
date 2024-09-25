12e80deb08ccfeb4074f61a471bced5c48be3ea5
Alissonps15 <alissonportodossantos@gmail.com>
2024-09-25 20:02:34 -0300

    Initial commit

---
+++ src/main/java/br/edu/famper/projetodeseguros/ProjetoDeSegurosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.famper.projetodeseguros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeSegurosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoDeSegurosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/famper/projetodeseguros/ProjetoDeSegurosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.projetodeseguros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeSegurosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
